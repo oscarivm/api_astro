8be4bacc04137e89f448f3f5e71528f061691bbb
oscarivm <56213369+oscarivm@users.noreply.github.com>
2024-05-04 01:02:14 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oscarivm.github.io',
+  site: 'https://oscarivm.github.io/api_astro/',
   integrations: [tailwind()]
 });
